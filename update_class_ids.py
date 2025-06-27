43b68090ba1e09bae7ac1e943d4395c01d2ebece
tuchan2002 <trananhtu171002@gmail.com>
2025-06-27 16:58:45 +0700

    first commit

---
+++ update_class_ids.py
@@ -0,0 +1,31 @@
+import os
+
+def update_class_ids(label_folder, old_id, new_id):
+    for dirpath, _, filenames in os.walk(label_folder):
+        for filename in filenames:
+            if not filename.endswith('.txt'):
+                continue
+
+            file_path = os.path.join(dirpath, filename)
+
+            with open(file_path, 'r') as f:
+                lines = f.readlines()
+
+            updated_lines = []
+            for line in lines:
+                parts = line.strip().split()
+                if len(parts) < 5:
+                    continue  # bỏ qua dòng lỗi
+
+                if parts[0] == str(old_id):
+                    parts[0] = str(new_id)
+                updated_lines.append(' '.join(parts) + '\n')
+
+            with open(file_path, 'w') as f:
+                f.writelines(updated_lines)
+
+    print(f"✅ Successfully updated class_id from {old_id} to {new_id} in {label_folder}")
+
+update_class_ids('datasets/corn/train/labels', 0, 80)
+update_class_ids('datasets/corn/valid/labels', 0, 80)
+update_class_ids('datasets/corn/test/labels', 0, 80)
